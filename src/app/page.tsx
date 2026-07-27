ec4b07677bfe69ff2204fad0bd114c582e1d1d67
Vishalsahani156 <vishalsahani158@gmail.com>
2026-07-27 23:17:51 +0530

    add the UI webanar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import HereSection from "@/components/HereSection";
 import FeatureCourses from "@/components/FeatureCourses";
 import MusicSchoolTestiMonials from "@/components/MusicSchoolTestiMonials";
+import UpcommingWebinars from "@/components/UpcommingWebinars";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HereSection />
       <FeatureCourses />
       <MusicSchoolTestiMonials/>
+      <UpcommingWebinars/>
     </main>
   );
 }
